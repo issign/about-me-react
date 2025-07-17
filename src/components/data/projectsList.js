5dbd8e74a241b0ac825bb8f3e5ab28e7e9f365be
KihoLee <kiho2734@naver.com>
2025-07-17 14:59:18 +0900

    [Update] Add test data for projects

---
+++ src/components/data/projectsList.js
@@ -0,0 +1,63 @@
+import portfolio1 from "../../../public/portfolio1.jpg";
+import portfolio2 from "../../../public/portfolio2.jpg";
+import portfolio3 from "../../../public/portfolio3.jpg";
+import portfolio4 from "../../../public/portfolio4.jpg";
+import portfolio5 from "../../../public/portfolio5.jpg";
+import portfolio6 from "../../../public/portfolio6.jpg";
+
+export const projectsList = [
+  {
+    id: 1,
+    image: portfolio1,
+    title: "Project 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, accusantium fugit sit doloremque saepe perferendis, qui et officia beatae autem quo repellendus adipisci maiores voluptatum deserunt aut? Dolores, molestiae quam.",
+    link: "https://github.com/issign",
+    technologies: ["React", "TypeScript", "Node"],
+  },
+  {
+    id: 2,
+    image: portfolio2,
+    title: "Project 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, accusantium fugit sit doloremque saepe perferendis, qui et officia beatae autem quo repellendus adipisci maiores voluptatum deserunt aut? Dolores, molestiae quam.",
+    link: "https://github.com/issign",
+    technologies: ["Python", "Docker", "Node"],
+  },
+  {
+    id: 3,
+    image: portfolio3,
+    title: "Project 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, accusantium fugit sit doloremque saepe perferendis, qui et officia beatae autem quo repellendus adipisci maiores voluptatum deserunt aut? Dolores, molestiae quam.",
+    link: "https://github.com/issign",
+    technologies: ["React", "TypeScript", "Node"],
+  },
+  {
+    id: 4,
+    image: portfolio4,
+    title: "Project 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, accusantium fugit sit doloremque saepe perferendis, qui et officia beatae autem quo repellendus adipisci maiores voluptatum deserunt aut? Dolores, molestiae quam.",
+    link: "https://github.com/issign",
+    technologies: ["React", "TypeScript", "Node"],
+  },
+  {
+    id: 5,
+    image: portfolio5,
+    title: "Project 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, accusantium fugit sit doloremque saepe perferendis, qui et officia beatae autem quo repellendus adipisci maiores voluptatum deserunt aut? Dolores, molestiae quam.",
+    link: "https://github.com/issign",
+    technologies: ["React", "TypeScript", "Node"],
+  },
+  {
+    id: 6,
+    image: portfolio6,
+    title: "Project 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, accusantium fugit sit doloremque saepe perferendis, qui et officia beatae autem quo repellendus adipisci maiores voluptatum deserunt aut? Dolores, molestiae quam.",
+    link: "https://github.com/issign",
+    technologies: ["React", "TypeScript", "Node"],
+  },
+];
