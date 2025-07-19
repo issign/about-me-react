bcf34ce86f2c2af77cc59795320b141cd4a9409e
KihoLee <kiho2734@naver.com>
2025-07-19 21:16:03 +0900

    [Update] Change little style of Button component

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -2,9 +2,9 @@ import className from "classnames";
 
 const Button = ({ children, primary }) => {
   const classes = className(
-    "px-3 py-1.5 cursor-pointer rounded hover:scale-105 transition-all duration-300",
+    "px-6 py-2 rounded hover:scale-105 transition-all duration-300",
     {
-      "color-primary text-white": primary,
+      "color-primary text-white font-bold": primary,
     }
   );
 
